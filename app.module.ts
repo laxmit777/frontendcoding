a32387c1a195ab4c2ef4db8d599c75114ddf1cfe
laxmit777 <71090929+laxmit777@users.noreply.github.com>
2020-09-10 14:47:14 -0400

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { EnrolleesTableComponent } from './enrollees-table/enrollees-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EnrolleesTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
